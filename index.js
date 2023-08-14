513aa6c5dfa97b9f4dc4eba7865ec79d5b00cc8c
Dmitry Mormul <macairmormul@MacBook-Air-Dmitry.local>
2023-08-14 14:36:15 +0200

    added DB

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 4000
+const db = "mongodb+srv://expansion4you:xH7Rd6ji1Ya413xm@cluster0.kgapqpo.mongodb.net/" 
 
 const app = express() 
 
-app.listen(PORT, () => {
-    console.log('Server has been started!');
-})
+async function start() { 
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        }) 
+        console.log('Connected to DB') 
+        app.listen(PORT, () => console.log('Server has been started'))
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1)
+    }
+}
+start()
